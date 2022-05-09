e54b32fbbd4a5d8a3f36497c0a6605d8b7c0fa33
claseProg <damclase2021@gmail.com>
2022-05-09 09:14:18 +0200

    version 2.0 configurando multa

--- src/biblioteca/Multa.java
+++ src/biblioteca/Multa.java
@@ -5,24 +5,55 @@
  */
 package biblioteca;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  *
  * @author iago leiros
  */
 public class Multa {
-    
+    SimpleDateFormat formato=new SimpleDateFormat("dd/MM/yyyy");
     Prestamo prestamo;
     
     Lector lector; 
    
-    private String fechaInicio;
+    private Date fechaInicio;
     
-    private String fechaFin;
+    private Date fechaFin;
     
     
-    public void calcularFechaFin(){
+    public void calcularFechaFin(String fecha,byte diasPrestamo){
+      boolean correcto=false;
+        do{
+        try{     
+        System.out.println("Introduce fecha en la que realizas el prestamo");
+        fecha=Entrada.lerString();
+                      
+        Date fechaF=formato.parse(fecha);
         
-       
+        correcto=true;
+        }catch(Exception e){
+            
+            System.out.println("Introduce una fecha valida, el formato es dd/mm/yyyy");
+            
+        }
+        }while(!correcto);
+        
+      
+      
+        correcto=false;
+        
+        do{
+        System.out.println("Introduce cuantos dias vas a tenerlo");
+        diasPrestamo=Entrada.lerByte();
+        if(diasPrestamo>30){
+            System.out.println("Se excede la cantidad de dias de prestamo, introduce un número menor a :"+diasPrestamo);
+        }else{
+            correcto=true;
+        }
+        
+        }while(!correcto);
         
     }
     
